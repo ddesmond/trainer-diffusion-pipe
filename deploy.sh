ba5ffb525991a653dc1d4cee4a765e60eb6f3352
Aleks <crni.aki@gmail.com>
2025-08-08 19:11:02 +0200

    init

--- deploy.sh
+++ deploy.sh
@@ -13,22 +13,29 @@ export PIP_ROOT_USER_ACTION=ignore
 
 apt update -y && apt install -y aria2
 
-echo "----------------------------------"
-echo "Setting up YUM YUM !"
-source ./structure.sh
-cd /opt
-
-
 echo "Run: Clone repo"
 git clone --depth 1 https://github.com/ddesmond/trainer-diffusion-pipe.git
 cd /opt/trainer-diffusion-pipe
 git pull
 
+
+echo "----------------------------------"
+echo "Setting up YUM YUM !"
+
+
+echo "----------------------------------"
+echo "Setting up structure !"
+cd /opt/trainer-diffusion-pipe
+source ./structure.sh
+ls -la /opt/
+
+
+
 source ./wan_download.sh &
 cd /opt/trainer-diffusion-pipe
 source ./setup.sh
 
-cp -r ./configs/ /opt/workspace/configs
+cp -r ./configs/ /opt/workspace/configs/
 
 cp /opt/trainer-diffusion-pipe/gradio_app.py /opt/diffusion-pipe/gradio_app.py
 cd /opt/diffusion-pipe
