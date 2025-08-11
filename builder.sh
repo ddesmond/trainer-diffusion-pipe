71e6d455a1875c4b6c648d1b820aa034b84ec4f9
Aleks <crni.aki@gmail.com>
2025-08-11 08:41:48 +0200

    init

--- builder.sh
+++ builder.sh
@@ -3,7 +3,7 @@
 mkdir -p /opt/miniconda3
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /opt/miniconda.sh
-bash /opt/miniconda.sh -b -p -u /opt/miniconda3
+bash /opt/miniconda.sh -b -u -p /opt/miniconda3
 
 source /opt/miniconda3/bin/activate  #(activates the conda)
 
@@ -13,11 +13,20 @@ echo 'export PATH="/opt/miniconda3/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 conda init --all
+source ~/.bashrc
 
 cd /opt/diffusion-pipe
+conda config --add channels conda-forge
+conda config --add channels defaults
 
-conda create -n diffusion-pipe python=3.12
+conda tos accept --override-channels --channel defaults
+conda tos accept --override-channels --channel conda-forge
+
+
+conda create -n diffusion-pipe python=3.12 --yes
 conda activate diffusion-pipe
+
+pip install --upgrade pip
 
 pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu128
 
